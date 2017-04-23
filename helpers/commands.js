f1200a7ac28f615bddc7295540dffac63cf2174e
ivrx <ivrxea@gmail.com>
2017-04-23 23:36:04 +0300

    add createInstance feature

--- helpers/commands.js
+++ helpers/commands.js
@@ -1,111 +1,212 @@
 const vultr = require('./vultr');
 const configuration = require('./configuration');
 const inquirer = require('inquirer');
+const _ = require('lodash');
 
 function displayInstances() {
 
-    let env = configuration.getEnv();
+  let env = configuration.getEnv();
 
-    vultr.listInstances(env).then((servers) => {
+  vultr.listInstances(env).then((servers) => {
 
-        for (let server of servers) {
+    for (let server of servers) {
 
-            console.log('====================');
-            console.log(`ID: ${server.SUBID}`);
-            console.log(`Status: ${server.status}, ${server.power_status}`);
-            console.log(`IP: ${server.main_ip}`);
-            console.log(`Location: ${server.location}`);
-            console.log(`OS: ${server.os}`);
-            console.log(`RAM: ${server.ram}`);
-            console.log(`Disk: ${server.disk}`);
-            console.log(`Label: ${server.label}`);
+      console.log('====================');
+      console.log(`ID: ${server.SUBID}`);
+      console.log(`Status: ${server.status}, ${server.power_status}`);
+      console.log(`IP: ${server.main_ip}`);
+      console.log(`Location: ${server.location}`);
+      console.log(`OS: ${server.os}`);
+      console.log(`RAM: ${server.ram}`);
+      console.log(`Disk: ${server.disk}`);
+      console.log(`Label: ${server.label}`);
 
-        }
-    })
+    }
+  })
 }
 
 function selectInstance(id) {
 
-    let env = configuration.getEnv();
+  let env = configuration.getEnv();
 
-    vultr.listInstances(env)
-        .then((servers) => {
+  vultr.listInstances(env)
+    .then((servers) => {
 
-            let targetInstance = false;
+      let targetInstance = false;
 
-            if (id) {
+      if (id) {
 
-                let instance = servers.find(server => server.SUBID == id);
-                if (!instance) {
-                    console.log('Wrong instance ID');
-                }
+        let instance = servers.find(server => server.SUBID == id);
+        if (!instance) {
+          console.log('Wrong instance ID');
+        }
 
-                configuration.modEnv('instanceID', instance.SUBID)
+        configuration.modEnv('instanceID', instance.SUBID)
 
-            } else {
+      } else {
 
-                let instances = [];
-                servers.map((server) => {
-                    instances.push(`[${server.SUBID}]: ${server.main_ip}, ${server.location}`);
-                });
+        let instances = [];
+        servers.map((server) => {
+          instances.push(`[${server.SUBID}]: ${server.main_ip}, ${server.location}`);
+        });
 
-                inquirer.prompt([{
-                    type: 'list',
-                    name: 'instance',
-                    message: 'Select an instance',
-                    choices: instances
-                }])
-                    .then((answer) => {
-                        targetInstance = /[0-9]+/.exec(answer.instance)[0];
-                        configuration.modEnv('instanceID', targetInstance);
-                    })
-            }
+        inquirer.prompt([{
+          type: 'list',
+          name: 'instance',
+          message: 'Select an instance',
+          choices: instances
+        }])
+          .then((answer) => {
+            targetInstance = /[0-9]+/.exec(answer.instance)[0];
+            configuration.modEnv('instanceID', targetInstance);
+          })
+      }
 
 
-        }).catch((e) => {
-        console.log(e);
-        return false;
-    });
+    }).catch((e) => {
+    console.log(e);
+    return false;
+  });
 
 
 }
 
 function showInstance(detail) {
 
-    let env = configuration.getEnv();
-    let instanceId = env.instanceID;
-    if (!instanceId) {
+  let env = configuration.getEnv();
+  let instanceId = env.instanceID;
+  if (!instanceId) {
 
-        console.log('Cannot get instanceID from config.json');
+    console.log('Cannot get instanceID from config.json');
+    return;
+
+  }
+
+  vultr.listInstances(env)
+    .then((servers) => {
+
+      let instance = servers.find(server => server.SUBID == instanceId);
+
+      if (!instance) {
+
+        console.log('Cannot find instance');
         return;
 
-    }
+      }
 
-    vultr.listInstances(env)
-        .then((servers) => {
+      console.log(instance[detail] || `No option for ${detail}`)
 
-            let instance = servers.find(server => server.SUBID == instanceId);
-
-            if (!instance) {
-
-                console.log('Cannot find instance');
-                return;
-
-            }
-
-            console.log(instance[detail] || `No option for ${detail}`)
-
-        })
-        .catch((e) => {
-            console.log(e);
-        })
+    })
+    .catch((e) => {
+      console.log(e);
+    })
 
 }
 
-function unselectInstance () {
+function unselectInstance() {
 
-    configuration.delEnv('instanceID');
-    console.log('Instance no longer selected');
+  configuration.delEnv('instanceID');
+  console.log('Instance no longer selected');
 
 }
-module.exports = {displayInstances, selectInstance, showInstance, unselectInstance};
+
+function createInstance() {
+
+  let env = configuration.getEnv();
+  let planChoices, regionsChoices, osChoices;
+  vultr.getPlansDetails(env)
+    .then(planDetails => {
+
+
+
+      let plans = vultr.objectsToArrayOfObjects(planDetails.plans);
+      let regions = vultr.objectsToArrayOfObjects(planDetails.regions);
+      let os = vultr.objectsToArrayOfObjects(planDetails.os);
+
+      planChoices = [];
+      plans
+        .filter(plan => !_.isEmpty(plan.available_locations))
+        .map((plan) => {
+        planChoices.push(`[${plan.VPSPLANID}]: ${plan.name}, ${plan.vcpu_count} VCPU, ${plan.price_per_month}$/m`)
+      });
+
+      let planId, regionId, osId;
+
+      inquirer.prompt({
+        type: 'list',
+        name: 'plan',
+        message: 'Select a plan',
+        choices: planChoices
+      }).then((answer) => {
+
+        planId = /[0-9]+/.exec(answer.plan)[0];
+
+        let regionsIds = plans[planId].available_locations;
+        regionsChoices = [];
+
+        if (!_.isEmpty(regionsIds)) {
+
+          regionsIds.map((id) => {
+            regionsChoices.push(`[${id}]: ${regions[id].name}, ${regions[id].country}`)
+          });
+
+        } else {
+
+          regions.map((region) => {
+            regionsChoices.push(`[${region.DCID}]: ${region.name}, ${region.country}`)
+          });
+
+        }
+
+        inquirer.prompt({
+          type: 'list',
+          name: 'region',
+          message: 'Select a region',
+          choices: regionsChoices
+        }).then((answer) => {
+
+          regionId = /[0-9]+/.exec(answer.region)[0];
+
+          osChoices = [];
+          os
+            .filter((sys) => sys.windows == false && sys.arch == 'x64' && sys.family == 'ubuntu')
+            .map((sys) => { osChoices.push(`[${sys.OSID}]: ${sys.name}`) });
+
+          inquirer.prompt({
+            type: 'list',
+            name: 'os',
+            message: 'Select an OS',
+            choices: osChoices
+          }).then((answer) => {
+
+            osId = /[0-9]+/.exec(answer.os)[0];
+
+            inquirer.prompt({
+              type: 'confirm',
+              name: 'create',
+              message: 'Are you sure?',
+              default: false
+
+            }).then((answer) => {
+
+              if (answer.create !== true) return;
+
+              vultr.createInstance(planId, regionId, osId, env)
+                .then((server) => {
+                console.log(`Instance created, ID: ${server.SUBID}`);
+              }).catch(e => console.log(e));
+
+            })
+
+          })
+
+        });
+      });
+
+    })
+    .catch(e => {
+      console.log(e);
+    });
+
+}
+module.exports = {displayInstances, selectInstance, showInstance, unselectInstance, createInstance};
--- helpers/vultr.js
+++ helpers/vultr.js
@@ -2,59 +2,130 @@ const _ = require('lodash');
 const Vultr = require('vultr');
 function checkVultrApi(env) {
 
-    let api = env.vultr;
-    return new Promise((fulfill, reject) => {
+  let api = env.vultr;
+  return new Promise((fulfill, reject) => {
 
-        if (env.validApi) {
-            fulfill();
-        }
+    if (env.validApi) {
+      fulfill();
+    }
 
-        let instance = new Vultr(api);
-        instance.account.info().then((info) => {
+    let instance = new Vultr(api);
+    instance.account.info().then((info) => {
 
-            if (!_.isEmpty(info)) {
-                fulfill(info)
-            }
+      if (!_.isEmpty(info)) {
+        fulfill(info)
+      }
 
-        }).catch(e => {
-            reject(e);
-        });
-
+    }).catch(e => {
+      reject(e);
     });
+
+  });
 }
 
 function listInstances(env) {
 
-    return new Promise((fulfill, reject) => {
+  return new Promise((fulfill, reject) => {
 
-        let instance = new Vultr(env.vultr);
-        instance.server.list()
-            .then((response) => {
+    let instance = new Vultr(env.vultr);
+    instance.server.list()
+      .then((response) => {
 
-                let servers = [];
+        let servers = [];
 
-                Object.keys(response).forEach((key) => {
-                    servers.push(response[key]);
-                });
+        Object.keys(response).forEach((key) => {
+          servers.push(response[key]);
+        });
 
-                fulfill(servers);
+        fulfill(servers);
 
-            })
-            .catch((e) => {
-                reject(e);
-            });
-    });
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
 
 
 }
 
 function selectInstance(env, id) {
 
-    if (id) {
-        console.log('selected id:', id)
-    } else {
-        console.log('no id selected')
-    }
+  if (id) {
+    console.log('selected id:', id)
+  } else {
+    console.log('no id selected')
+  }
 }
 
-module.exports = {checkVultrApi, listInstances, selectInstance};
+function getPlansDetails(env) {
+
+  return new Promise((fulfill, reject) => {
+
+    let api = env.vultr;
+    let instance = new Vultr(api);
+
+    let plans = instance.plans.list().then(plans => plans);
+    let regions = instance.regions.list().then(regions => regions);
+    let os = instance.os.list().then(os => os);
+
+    Promise.all([plans, regions, os])
+      .then(values => {
+        fulfill({
+          plans: values[0],
+          regions: values[1],
+          os: values[2]
+        });
+      })
+      .catch(e => {
+        reject(e)
+      })
+  });
+}
+
+function objectsToArrayOfObjects(obj) {
+
+  let arr = [];
+  Object.keys(obj).forEach((key) => {
+
+    arr[key] = obj[key];
+
+  });
+
+  return arr;
+
+}
+
+function createInstance(planId, regionId, osId, env) {
+
+  return new Promise((fulfill, reject) => {
+
+    let api = env.vultr;
+    let vultrInstance = new Vultr(api);
+
+    vultrInstance.server.create({
+      plan: planId,
+      os: osId,
+      region: regionId
+
+    })
+      .then((res) => {
+        fulfill(res);
+      })
+      .catch(e => {
+        reject(e);
+      })
+
+
+  });
+
+
+}
+
+module.exports = {
+  checkVultrApi,
+  listInstances,
+  selectInstance,
+  getPlansDetails,
+  objectsToArrayOfObjects,
+  createInstance
+};
